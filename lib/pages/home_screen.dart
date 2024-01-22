b33385e99e2bc496db386ff27df4ffca7f5e5a5c
Arnaldo Junior <abs.junnior@hotmail.com>
2024-01-22 17:56:39 -0300

    Mudança no layot superior

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -55,91 +55,104 @@ class _HomeScreenState extends State<HomeScreen> {
         altura: alturaUsuario,
         dadosIMCRepository: dadosIMCRepository,
       ),
-      backgroundColor: CustomColors().getGradientMainColor(),
+      backgroundColor: CustomColors().getCircleColor(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             const SizedBox(height: 50),
+            const Text(
+              "Calculadora IMC",
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 29),
+              child: Row(
                 children: [
-                  InkWell(
-                    onTap: () {},
-                    child: const Icon(
-                      Icons.more_vert,
-                      color: Colors.white,
-                      size: 25,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: FileImage(
-                            File(photo),
-                          ),
-                          radius: 55,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          nomeUsuario,
-                          style: const TextStyle(
-                            fontSize: 23,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          "$alturaUsuario cm",
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 160, 75, 75),
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                  Card(
+                    elevation: 5,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      height: 170,
+                      width: 340,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
                           children: [
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: const BoxDecoration(
-                                color: Color(0xFF9F97E2),
-                                shape: BoxShape.circle,
+                            CircleAvatar(
+                              backgroundImage: FileImage(
+                                File(photo),
                               ),
-                              child: const Icon(
-                                Icons.call,
-                                color: Colors.white,
-                                size: 25,
-                              ),
+                              radius: 55,
                             ),
-                            const SizedBox(width: 20),
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: const BoxDecoration(
-                                color: Color(0xFF9F97E2),
-                                shape: BoxShape.circle,
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 2,
                               ),
-                              child: const Icon(
-                                Icons.call,
-                                color: Colors.white,
-                                size: 25,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    nomeUsuario,
+                                    style: const TextStyle(
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Text("Altura:"),
+                                      Text(
+                                        "$alturaUsuario cm",
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              Color.fromARGB(255, 160, 75, 75),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Text("Sexo:"),
+                                      Text(
+                                        "$alturaUsuario",
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              Color.fromARGB(255, 160, 75, 75),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Card(
+                                    elevation: 3,
+                                    child: Container(
+                                      height: 65,
+                                      width: 180,
+                                      decoration: BoxDecoration(
+                                        color: Color.fromARGB(255, 214, 207, 186),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(2),
+                                        child: Text("uahuahauhua uahuah uahauh  uahuauah uahua  uahuahuah  auhauha "),
+                                      ),
+                                    ),
+                                  )
+                                ],
                               ),
-                            ),
+                            )
                           ],
-                        )
-                      ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 20),
             Container(
               height: MediaQuery.of(context).size.height / 1.3,
               width: double.infinity,
@@ -150,8 +163,8 @@ class _HomeScreenState extends State<HomeScreen> {
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(45),
-                  topRight: Radius.circular(45),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
               ),
               child: Column(
